e80a6b293f451099d8496f01742d15f83b99d3da
Marcos Paulo <30324635+souzamp@users.noreply.github.com>
2024-11-05 23:06:16 -0300

    Trabalhando com Models
    Adicionando Typescript ao projeto, criando a classe modelo do componente Porta.ts

--- components/Porta.tsx
+++ components/Porta.tsx
@@ -1,14 +1,20 @@
+import PortaModel from "../model/porta";
 import styles from "../styles/Porta.module.css";
 
+interface PortaProps {
+    porta: PortaModel
+}
+
 export default function Porta(props) {
+    const {porta} = props
     // podemos receber o valor de qual propriedade usar no CSS via parametro
-    const selecionada = props.selecionada ? styles.porta_selecionada : ''
+    const selecionada = porta.selecionada ? styles.porta_selecionada : ''
 
     return (
         <div className={styles.area}>
             <div className={`${styles.grade} ${selecionada}` }>
                 <div className={styles.porta}>
-                    <div className={styles.numero}>3</div>
+                    <div className={styles.numero}>{porta.numero}</div>
                     <div className={styles.marcaneta}></div>
                 </div>
             </div>
---
+++ model/porta.ts
@@ -0,0 +1,52 @@
+export default class Porta {
+    // # usamos para definir que o atributo é privado em javascript
+    // estamos definiando nossos atributos
+    #numero: number
+    #temPresente: boolean
+    #selecionada: boolean
+    #aberta: boolean
+
+    // vamos criar nosso construtor de classe
+    constructor(numero, temPresente = false, selecionada = false, aberta = false) {
+        this.#numero = numero
+        this.#temPresente = temPresente
+        this.#selecionada = selecionada
+        this.#aberta = aberta
+    }
+
+    // precisamos criar nos Gets
+    get numero() {
+        return this.#numero
+    }
+
+    get temPresente() {
+        return this.#temPresente
+    }
+
+    get selecionada() {
+        return this.#selecionada
+    }
+
+    get aberta() {
+        return this.#aberta
+    }
+
+    alternarSelecao() {
+        // estamos alterando o valor selecionado
+        const selecionada = !this.#selecionada
+
+        // para não alterar o estado da nossa instância vamos retornar uma nova apenas com a
+        // modificacao que fizemos
+        return new Porta(this.numero, this.temPresente, selecionada, this.aberta)
+    }
+
+    desmarcarPorta() {
+        const selecionada = false
+        return new Porta(this.numero, this.temPresente, selecionada, this.aberta)
+    }
+
+    abrir() {
+        const aberta = true
+        return new Porta(this.numero, this.temPresente, this.selecionada, aberta)
+    }
+}
